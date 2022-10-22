578ccf9ae31d67a6c9557e6128b1c2fa1681af14
Dimitrija Timeski <dimitrijatimeskidimitrija@gmail.com>
2022-10-22 19:24:49 +0200

    Session management

--- src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
@@ -1,6 +1,7 @@
 package mk.ukim.finki.wpaud.bootstrap;
 
 import mk.ukim.finki.wpaud.model.Category;
+import mk.ukim.finki.wpaud.model.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,9 +11,16 @@ import java.util.List;
 @Component
 public class DataHolder {
     public static List<Category> categoryList = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
+
+
     @PostConstruct
     public void init() {
         this.categoryList.add(new Category("Software", "Software Category"));
         this.categoryList.add(new Category("Books", "Books Category"));
+
+        users.add(new User("dimitrija.timeski", "dt", "Dimitrija", "Timeski"));
+        users.add(new User("tomislav.stojceski", "ts", "Tomislav", "Stojceski"));
+        users.add(new User("branka.tasovska.stojceska", "bts", "Branka", "Tasovska"));
     }
 }
