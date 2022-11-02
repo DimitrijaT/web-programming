1179ee3ec952e100c0e66d861b9a0c7c408d390d
Dimitrija Timeski <dimitrijatimeskidimitrija@gmail.com>
2022-11-03 00:54:14 +0100

    MVC pattern. Login, logout, register implementation.

--- src/main/java/mk/ukim/finki/wpaud/web/servlet/LogoutServlet.java
+++ src/main/java/mk/ukim/finki/wpaud/web/servlet/LogoutServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Logout Servlet", urlPatterns = "/logout")
+@WebServlet(name = "Logout Servlet", urlPatterns = "/servlet/logout")
 public class LogoutServlet extends HttpServlet {
 
     @Override
